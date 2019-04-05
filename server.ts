973eb3ed42c181f6090b0af4909ac2b9e53fa410
par.nordstrom <par.nordstrom@valtech.se>
2019-04-05 14:19:36 +0200

    Add long-endpoint

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { readFileSync } from 'fs';
 // Create a new express application instance
 const app: express.Application = express();
 
-const getShortData = (url: string) => {
+const getData = (url:string) => {
   let data = readFileSync('./data.json').toString();
   var urls = JSON.parse(data);
   return urls[url];
@@ -17,11 +17,22 @@ app.get('/', function(req, res) {
 
 app.get('/verified', isVerified);
 
-app.get('/short/', function(req, res) {
+// Is short/long the right naming scheme?
+app.get('/short/', function(req, res) { 
   const url = req.query.url;
-  const shortData = getShortData(url);
-  if (shortData == undefined) res.sendStatus(404); // Perhaps wrong to return 404 here...
-  res.send({ url: url, vote: shortData });
+  const data = getData(url);
+  if(data == undefined)
+    res.sendStatus(404); // Perhaps wrong to return 404 here... 
+  res.send({url:url, vote: data.vote});
+});
+
+// Is short/long the right naming scheme?
+app.get('/long/', function(req, res) {
+  const url = req.query.url;
+  const data = getData(url);
+  if(data == undefined)
+    res.sendStatus(404); // Perhaps wrong to return 404 here... 
+  res.send({url:url, vote: data.vote, links: data.links });
 });
 
 app.listen(3000, function() {
