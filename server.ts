e6a1c16efc6fde9c9ca3a593d1c7cb2068148994
par.nordstrom <par.nordstrom@valtech.se>
2019-04-05 13:23:37 +0200

    Add short-endpoint

--- server.ts
+++ server.ts
@@ -7,6 +7,13 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/short/', function(req, res) {
+  const url = req.query.url;
+  const shortData = {pro: 21, con:4};
+  res.send(shortData);
+});
+
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
