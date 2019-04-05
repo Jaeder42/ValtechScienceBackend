5c3c8947b327d6313beea19058279aa5c534f630
Johan Jäderblom <johan.jaderblom@valtech.se>
2019-04-05 21:31:09 +0200

    get data from session data

--- handlers/getData.ts
+++ handlers/getData.ts
@@ -1,18 +1,33 @@
 import { Request, Response } from 'express';
-import urls from '../utils/session-data.json';
+import { readFileSync } from 'fs';
 
 export const getShortData = async (req: Request, res: Response) => {
-  const { url } = req.query;
-  const data = urls.find(d => d.url == url);
-  if (data == undefined) res.sendStatus(404);
-  // Perhaps wrong to return 404 here...
-  else res.send({ url: url, votes: data.votes });
+  try {
+    const urls = JSON.parse(
+      readFileSync('./utils/session-data.json').toString()
+    );
+    const { url } = req.query;
+    const data = urls.find((d: any) => d.url == url);
+    if (data == undefined) res.sendStatus(404);
+    // Perhaps wrong to return 404 here...
+    else res.send({ url: url, votes: data.votes });
+  } catch (err) {
+    res.sendStatus(500);
+  }
 };
 
 export const getLongData = async (req: Request, res: Response) => {
-  const { url } = req.query;
-  const data = urls.find(d => d.url == url);
-  if (data == undefined) res.sendStatus(404);
-  // Perhaps wrong to return 404 here...
-  else res.send({ url: url, votes: data.votes, links: data.links });
+  try {
+    const urls = JSON.parse(
+      readFileSync('./utils/session-data.json').toString()
+    );
+
+    const { url } = req.query;
+    const data = urls.find((d: any) => d.url == url);
+    if (data == undefined) res.sendStatus(404);
+    // Perhaps wrong to return 404 here...
+    else res.send({ url: url, votes: data.votes, links: data.links });
+  } catch (err) {
+    res.sendStatus(500);
+  }
 };
