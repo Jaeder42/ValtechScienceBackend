9544ac72f9818f77e7055726b03d99419c18acc7
par.nordstrom <par.nordstrom@valtech.se>
2019-04-05 13:57:16 +0200

    Handle a non existing url

--- server.ts
+++ server.ts
@@ -7,8 +7,7 @@ const app: express.Application = express();
 const getShortData = (url:string) => {
   let data = readFileSync('./data.json').toString();
   var urls = JSON.parse(data);
-  const result = urls[url]; 
-  return ({ url: url, votes : result });
+  return urls[url]; 
 } 
 
 app.get('/', function(req, res) {
@@ -18,7 +17,9 @@ app.get('/', function(req, res) {
 app.get('/short/', function(req, res) {
   const url = req.query.url;
   const shortData = getShortData(url);
-  res.send(shortData);
+  if(shortData == undefined)
+    res.sendStatus(404); // Perhaps wrong to return 404 here... 
+  res.send({url:url, vote: shortData});
 });
 
 app.listen(3000, function() {
