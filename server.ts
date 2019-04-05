d9144c4969e9fe47eb0131f1f09647fda7c9321b
par.nordstrom <par.nordstrom@valtech.se>
2019-04-05 13:24:44 +0200

    Break out getShortData

--- server.ts
+++ server.ts
@@ -3,16 +3,17 @@ import express = require('express');
 // Create a new express application instance
 const app: express.Application = express();
 
+const getShortData = (url:string) =>  ({ url: url, pro: 21, con: 4 });
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
 app.get('/short/', function(req, res) {
   const url = req.query.url;
-  const shortData = {pro: 21, con:4};
+  const shortData = getShortData(url);
   res.send(shortData);
 });
-
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
